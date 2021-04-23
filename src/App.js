7a173d2d2614590b19e63a4f8e0987bc28fe4e72
Abhiram Satpute <abhiram.satpute@gmail.com>
2021-04-24 03:57:03 +0530

    Added multiple CSS features and updated how the Rows feel and function, diff netflix originals vs others

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
-import Row from './Row';
-import requests from './requests';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import Row from "./Row";
+import requests from "./requests";
 
 function App() {
-
-  return(
-  <div className="App">
-    <Row title ="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals}/>
-    <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
-  </div>);
+  return (
+    <div className="App">
+      <Row
+        title="Netflix Originals"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+    </div>
+  );
 }
 
 export default App;
--- src/Row.js
+++ src/Row.js
@@ -1,50 +1,54 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 import axios from "./axios";
-import requests from './requests';
-//re-listen 29:10 of the tutorial!
+import requests from "./requests";
+//re-listen 29:10, 54:40 of the tutorial!
+// very important isLArgeRo!!!!!!!!!!
+import "./Row.css";
 
 const baseUrl = "https://image.tmdb.org/t/p/original/";
 
-
-function Row ( {title, fetchUrl} ) {
-
-    
+function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
-//when the row appears on screen, i make a PULL req to tmdb RIGHT when the row loads
-//categories are based on the fetchURL
-//so when row loads, we run this useEffect
-  useEffect( () => {      
-// if empty [], then run once when row loads, and never run again, if [abcd], then run once, then every time if abcd changes
-  
-//We cant make useEffect as async, so make a function inside useEffect and make it ASYNC!
-        async function fetchData() {
+  //when the row appears on screen, i make a PULL req to tmdb RIGHT when the row loads
+  //categories are based on the fetchURL
+  //so when row loads, we run this useEffect
+  useEffect(() => {
+    // if empty [], then run once when row loads, and never run again, if [abcd], then run once, then every time if abcd changes
 
-            const request = await axios.get(fetchUrl);
-            //basically appen+ding
-            console.log(request.data.results); // to check the data structure that we get.
-            setMovies(request.data.results);
-            return request;
-        }
+    //We cant make useEffect as async, so make a function inside useEffect and make it ASYNC!
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      //basically appen+ding
+      console.log(request.data.results); // to check the data structure that we get.
+      setMovies(request.data.results);
+      return request;
+    }
 
     fetchData();
+  }, [fetchUrl]); // very very important!!!!!!!!!! if different url passed, so it wouldn't re-renders
+  //fetchUrl is outside the block
 
-    }, [fetchUrl] ) // very very important!!!!!!!!!! if different url passed, so it wouldn't re-renders
-    //fetchUrl is outside the block
+  console.log(movies);
+  // they are hori scrollables
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row_posters">
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+            src={`${baseUrl}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
 
-    console.log(movies)
-    // they are hori scrollables
-    return (
-        <div className="row">
-            <h2>{title}</h2>
-            <div className="row_posters">
-                {movies.map(movie => (
-                    <img src={`${baseUrl}${movie.poster_path}`} alt={movie.name} />
-                    ))}
-            </div>
-
-            {/* it will have title (NF originals, trending, category, etc.) then have Container containing the bunch of posters */}
-        </div>
-    )
+      {/* it will have title (NF originals, trending, category, etc.) then have Container containing the bunch of posters */}
+    </div>
+  );
 }
 
 export default Row;
