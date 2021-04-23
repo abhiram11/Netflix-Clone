a9040b697b21d76109dd12ca4848fb6f61b6e400
Abhiram Satpute <abhiram.satpute@gmail.com>
2021-04-24 02:25:09 +0530

    added movie images with alt as the movie name, no styling so posters are huge

--- src/Row.js
+++ src/Row.js
@@ -3,6 +3,9 @@ import axios from "./axios";
 import requests from './requests';
 //re-listen 29:10 of the tutorial!
 
+const baseUrl = "https://image.tmdb.org/t/p/original/";
+
+
 function Row ( {title, fetchUrl} ) {
 
     
@@ -14,21 +17,31 @@ function Row ( {title, fetchUrl} ) {
 // if empty [], then run once when row loads, and never run again, if [abcd], then run once, then every time if abcd changes
   
 //We cant make useEffect as async, so make a function inside useEffect and make it ASYNC!
-async function fetchData() {
+        async function fetchData() {
 
-    const request = await axios.get(fetchUrl);
-    //basically appen+ding
-    console.log(request); // to check the data structure that we get.
-    return request;
-}
+            const request = await axios.get(fetchUrl);
+            //basically appen+ding
+            console.log(request.data.results); // to check the data structure that we get.
+            setMovies(request.data.results);
+            return request;
+        }
 
-fetchData();
+    fetchData();
 
-}, [] )
+    }, [fetchUrl] ) // very very important!!!!!!!!!! if different url passed, so it wouldn't re-renders
+    //fetchUrl is outside the block
 
+    console.log(movies)
+    // they are hori scrollables
     return (
-        <div>
+        <div className="row">
             <h2>{title}</h2>
+            <div className="row_posters">
+                {movies.map(movie => (
+                    <img src={`${baseUrl}${movie.poster_path}`} alt={movie.name} />
+                    ))}
+            </div>
+
             {/* it will have title (NF originals, trending, category, etc.) then have Container containing the bunch of posters */}
         </div>
     )
