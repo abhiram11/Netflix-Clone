916ea1c7cf5ae072d44af0d2c51644b0691fa29a
Abhiram Satpute <abhiram.satpute@gmail.com>
2021-04-25 22:25:41 +0530

    added styles for banner

--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "./axios";
 import requests from "./requests";
+import "./Banner.css";
 
 function Banner() {
   const [movie, setMovie] = useState([]); //since refresh gives new movie
@@ -21,7 +22,26 @@ function Banner() {
   console.log(movie);
 
   return (
-    <header>
+    <header
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url("http://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+        backgroundPosition: "center center",
+      }}
+    >
+      <div className="banner_contents">
+        <h1 className="banner_title">
+          {/* find edge cases below, replace GIANT IF ELSE Statements! ? is called optional chaining! */}
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+        <div className="banner_buttons">
+          <button className="banner_button">Play</button>
+          <button className="banner_button">My List</button>
+        </div>
+        <h1 className="banner_description">{movie?.overview}</h1>
+      </div>
+
       {/* header has bgImage, title, div > 2 buttons, descriptions */}
     </header>
   );
